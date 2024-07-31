fd1e4fc133287c768334929a46d7d25e462c6ac5
graciofilipe <gracio.contact@gmail.com>
2024-07-31 11:24:12 +0000

    moved things from arguments to secrets

--- main.py
+++ main.py
@@ -2,6 +2,33 @@ from google.cloud import secretmanager
 from data_processing import iterate_over_calls, create_dataframe, update_df_and_save
 import argparse
 from datetime import datetime
+from google.cloud import secretmanager
+from datetime import datetime
+
+
+def get_bucket_name(project_id, version_id="latest"):
+
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{project_id}/secrets/restaurant_bucket_name/versions/{version_id}"
+    response = client.access_secret_version(request={"name": name})
+    return response.payload.data.decode("UTF-8")
+
+
+def get_coordinates(project_id, version_id="latest"):
+
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+    
+    top_left = f"projects/{project_id}/secrets/restaurant_top_left/versions/{version_id}"
+    top_left_response = client.access_secret_version(request={"name": name})
+
+    bottom_right = f"projects/{project_id}/secrets/restaurant_bottom_right/versions/{version_id}"
+    bottom_right_response = client.access_secret_version(request={"name": name})
+
+
+    return top_left_response.payload.data.decode("UTF-8"), bottom_right_response.payload.data.decode("UTF-8")
+
 
 
 def build_lat_long_grid(top_left, bottom_right, n_steps):
@@ -38,22 +65,22 @@ if __name__ == '__main__':
 
     # get the project id from environment variable: 
     project_id = os.environ.get('PROJECT_ID')
-    
+    restaurant_bucket_name = get_bucket_name(project_id=project_id, version_id="latest")
+    top_left, bottom_right = get_coordinates(project_id=project_id, version_id="latest")
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("--project_id", required=True)
-    parser.add_argument("--bucket_name", required=True)
-    parser.add_argument("--top_left", required=True)
-    parser.add_argument("--bottom_right", required=True)
     parser.add_argument("--n_steps", required=True)
     
     args = parser.parse_args()
 
-    lat_long_grid = build_lat_long_grid(string_to_tuple(args.top_left),
-                                        string_to_tuple(args.bottom_right),
+    lat_long_grid = build_lat_long_grid(string_to_tuple(top_left),
+                                        string_to_tuple(bottom_right),
                                         int(args.n_steps))
+
     restaurants = iterate_over_calls(lat_long_grid, project_id=project_id)
+    
     df_new = create_dataframe(restaurants)
-    update_df_and_save(df_new, args.bucket_name)
+    update_df_and_save(df_new, restaurant_bucket_name)
   
 
 
