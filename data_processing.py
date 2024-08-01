e408435749322472b6c1248018c2058ce29f8c69
graciofilipe <gracio.contact@gmail.com>
2024-08-01 10:02:46 +0000

    typo in datetime

--- data_processing.py
+++ data_processing.py
@@ -37,7 +37,7 @@ def iterate_over_calls(lat_long_pairs, project_id):
 
     restaurants = {}
     
-    today = datetime.date.today()
+    today = datetime.today()
     formatted_date = today.strftime("%Y-%m-%d")
 
     for rank in ["DISTANCE", "POPULARITY"]:
