2bccd22502d75178d6cf7ea18f6954449f735398
graciofilipe <gracio.contact@gmail.com>
2024-11-06 09:12:18 +0000

    testing the build

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='common_utils',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Shared utility functions for the restaurant project',
+    # Add other metadata as needed (author, license, etc.)
+)
