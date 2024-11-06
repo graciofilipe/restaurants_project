64708fb4a983ca4b16154c59ed68a1da2f3740c1
graciofilipe <gracio.contact@gmail.com>
2024-11-06 15:34:23 +0000

    fixing more typos

--- run_restaurant_finder/maps_call.py
+++ run_restaurant_finder/maps_call.py
@@ -17,13 +17,14 @@ def send_request(lat, long, radius, rank, API_KEY):
         "korean_restaurant", "lebanese_restaurant", "mediterranean_restaurant", "mexican_restaurant",
         "middle_eastern_restaurant", "pizza_restaurant",  "ramen_restaurant", "restaurant", 
         "seafood_restaurant", "spanish_restaurant", "steak_house", "sushi_restaurant", "thai_restaurant",
-        "turkish_restaurant", "vegetarian_restaurant", "vietnamese_restaurant"]
-    
+        "turkish_restaurant", "vegetarian_restaurant", "vietnamese_restaurant"
+        ],
     "excludedPrimaryTypes": ["meal_delivery", "meal_takeaway", "fast_food_restaurant",
-     "bar", "bakery", "cafe", "coffee_shop", "breakfast_restaurant", 
-     "ice_cream_shop", "sandwich_shop", "brunch_restaurant", "vegan_restaurant"]
+        "bar", "bakery", "cafe", "coffee_shop", "breakfast_restaurant", ,
+        "ice_cream_shop", "sandwich_shop", "brunch_restaurant", "vegan_restaurant"
+        ],
     "excludedTypes": ["bakery", "bar", "cafe", "coffee_shop", "breakfast_restaurant", 
-     "ice_cream_shop", "sandwich_shop", "brunch_restaurant", "vegan_restaurant"]
+        "ice_cream_shop", "sandwich_shop", "brunch_restaurant", "vegan_restaurant"],
     "locationRestriction": {
         "circle": {
         "center": {
