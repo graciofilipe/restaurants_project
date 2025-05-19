b5e0b6c8ae81c2370a3c9ffafa58f8a9e3b07fac
graciofilipe <gracio.contact@gmail.com>
2025-05-19 10:08:17 +0000

    comment old include and exclude types

--- restaurant_finder/config.py
+++ restaurant_finder/config.py
@@ -1,3 +1,22 @@
+    # "includedPrimaryTypes": ["afghani_restaurant", "african_restaurant", "american_restaurant", "asian_restaurant",
+    #     "barbecue_restaurant", "brazilian_restaurant", "buffet_restaurant", "chinese_restaurant", "french_restaurant",
+    #     "greek_restaurant", "hamburger_restaurant", "indian_restaurant", "indonesian_restaurant", "italian_restaurant",
+    #     "japanese_restaurant", "korean_restaurant", "lebanese_restaurant", "mediterranean_restaurant", "mexican_restaurant",
+    #     "middle_eastern_restaurant", "pizza_restaurant", "ramen_restaurant", "restaurant", "seafood_restaurant",
+    #     "spanish_restaurant", "steak_house", "sushi_restaurant", "thai_restaurant", 
+    #     "turkish_restaurant", "vegetarian_restaurant", "vietnamese_restaurant"],
+    # "excludedPrimaryTypes": ["acai_shop", "bagel_shop", "bakery", "bar", "bar_and_grill", "breakfast_restaurant",
+    #     "brunch_restaurant", "cafe", "cafeteria", "candy_store", "cat_cafe", "chocolate_factory", "chocolate_shop", "coffee_shop",
+    #     "confectionery", "deli", "dessert_restaurant", "dessert_shop", "dog_cafe", "donut_shop", "fast_food_restaurant",
+    #     "fine_dining_restaurant", "food_court", "ice_cream_shop", "juice_shop", "meal_delivery", "meal_takeaway", "pub",
+    #     "sandwich_shop", "tea_house", "vegan_restaurant", "wine_bar"],
+    # "excludedTypes": ["acai_shop", "bagel_shop", "bakery", "bar", "candy_store", "cat_cafe", "chocolate_factory", 
+    #     "chocolate_shop", "coffee_shop", "dessert_restaurant", "dessert_shop", "dog_cafe", "donut_shop",
+    #     "fast_food_restaurant", "food_court", "ice_cream_shop", "juice_shop", "pub",
+    #     "sandwich_shop", "tea_house","wine_bar"],
+
+
+
 INCLUDED_PRIMARY_TYPES = [
     "restaurant",
     "meal_delivery",
